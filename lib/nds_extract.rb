d39d13ce5f77258e22c7888e9c172e2c43949f6e
patrick hoehn <patrh_99@hotmail.com>
2019-11-11 22:53:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,7 +14,7 @@ def directors_totals(nds)
     director_data = nds[director_index]
     
     result[director_data][:name]=gross_for_director(director_data)
-    index += 1
+    director_index += 1
   end 
   result
 end
